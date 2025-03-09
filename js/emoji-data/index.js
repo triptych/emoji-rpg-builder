65b760ed125caaac8a2ac8c907c0e01a342b5562
Andrew Wooldridge <triptych@gmail.com>
2025-03-08 16:24:43 -0800

    Add objects and symbols emoji data files and update index.js to include them

--- js/emoji-data/index.js
+++ js/emoji-data/index.js
@@ -8,6 +8,8 @@ import natureEmojis from './nature.js';
 import foodEmojis from './food.js';
 import activitiesEmojis from './activities.js';
 import travelEmojis from './travel.js';
+import objectsEmojis from './objects.js';
+import symbolsEmojis from './symbols.js';
 
 const emojiData = {
     categories: {
@@ -30,6 +32,14 @@ const emojiData = {
         travel: {
             name: "Travel & Places",
             emojis: travelEmojis
+        },
+        objects: {
+            name: "Objects",
+            emojis: objectsEmojis
+        },
+        symbols: {
+            name: "Symbols",
+            emojis: symbolsEmojis
         }
     }
 };
